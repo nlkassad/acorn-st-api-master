66378d965953f8604878318fd89466f127a22120
nlkassad <nlkassad@hotmail.com>
2016-11-08 20:30:38 -0500

    Change to just gh source url

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module AcornStApiMaster
     cors_port = 'GA'.each_byte.reduce('') { |a, e| a + format('%d', e) }.to_i
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_ORIGIN'] || "https://nlkassad.github.io/acorn-st-ember-client"
+        origins ENV['CLIENT_ORIGIN'] || "https://nlkassad.github.io"
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
